c8af3cad393239f84e842eb2d001e3e07d31d60a
Baileigh <brownbaileigh1@gmail.com>
2022-08-19 16:11:16 -0400

    changed variable

--- includes/connection.php
+++ includes/connection.php
@@ -12,7 +12,7 @@
     $con = mysqli_connect($servername, $username, $password,$db);
     
     // Check connection
-    if (!$conn) {
+    if (!$con) {
       die("Connection failed: " . mysqli_connect_error());
     }
     echo "Connected successfully";
