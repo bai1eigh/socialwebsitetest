6286123cd09acad03319e35719492d97508c16db
Baileigh <brownbaileigh1@gmail.com>
2022-08-22 12:35:24 -0400

    fixing connection

--- includes/connection.php
+++ includes/connection.php
@@ -10,7 +10,7 @@
    //
    //// Create connection
    $con = mysqli_connect($servername, $username, $password,$db);
-   ////
+   //
    ////// Check connection
    if (!$con) {
      die("Connection failed: " . mysqli_connect_error());
